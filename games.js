edf662cb8da1ed8cad36924149dfd39a863d918b
Alain Joseph <alainrjoseph@gmail.com>
2019-03-11 18:16:51 -0400

    changes

--- games.js
+++ games.js
@@ -1,34 +1,40 @@
 var getLetter;
-var computerChoices = abcdefghijlmnopqrstuvwxyz;
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var wins = 0;
 var losses = 0;
 var numGuesses = 10;
-var Guessesleft;
+var guessesLeft;
 var userChoice = 0;
+var totalEntry = 0;
 
-window.addEventListener("onkeydown", function(event) {
 
+
+console.log("in JS")
+document.onkeydown=function(event){
+
+ 
+    console.log("in keydown")
     var userChoice = event.key;
 
-if (numGuesses > 0){  
-    getLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-}
+    if (numGuesses > 0) {
+        getLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    }
 
-    for (i=0; i<computerChoices.length; i++) {
+    for (i = 0; i < computerChoices.length; i++) {
         userChoice = ("");
-    if (userChoice === computerChoices) {
-    winN++;
-    } 
-    else {
-    loseN++;}
+        if (userChoice === computerChoices) {
+            wins++;
+        } else {
+            losses++;
+        }
 
-totalEntry.push(userChoice);                    
-Guessesleft--;
+        totalEntry.push(userChoice);
+     guessesLeft--;
 
-document.getElementById("userwin").innerHTML = wins;
-document.getElementById("userlose").innerHTML = losses;
-document.getElementById("userguess").innerHTML = GuessesLeft;
-document.getElementById("userguesslist").innerHTML = numGuesses;  
-    
+        document.getElementById("userwin").innerHTML = wins;
+        document.getElementById("userlose").innerHTML = losses;
+        document.getElementById("userguess").innerHTML = guessesLeft;
+        document.getElementById("userguesslist").innerHTML = numGuesses;
+
     }
 }
